13056ea5ee1ba3a64517cc61670182666480b71a
Reggie Burnett <reggie.burnett@oracle.com>
2012-07-20 11:07:13 -0500

    added a custom settings provider so the settings file appears in a sane place

---
+++ Source/NotifierSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySQL.Utility;
+using MySql.Notifier.Properties;
+
+namespace MySql.Notifier
+{
+  public class NotifierSettings : CustomSettingsProvider
+  {
+    public override string ApplicationName
+    {
+      get { return Resources.AppName; }
+      set { } 
+    }
+
+    public override string SettingsPath
+    {
+      get { return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Oracle\MySQL Notifier\settings.config"; }
+    }
+  }
+}
