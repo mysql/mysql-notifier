ac8571e4b63daf481a9d53f291b580420764fb66
Javier Treviño <javier.trevino@oracle.com>
2013-04-14 10:56:59 -0500

    Work Item #459 - Save monitored MySQL instances in settings file and update Notifier main UI to accomodate for monitored instances.
    - Added MySQLInstance and MySQLInstancesList classes to manage monitored MySQL instances. (Source/MySQLInstance.cs, Source/MySQLInstancesList.cs)

---
+++ Source/MySQLInstance.cs
@@ -0,0 +1,150 @@
+﻿//
+// Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; version 2 of the
+// License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301  USA
+//
+
+namespace MySql.Notifier
+{
+  using System;
+  using System.Linq;
+  using System.Xml;
+  using System.Xml.Serialization;
+  using MySQL.Utility;
+
+  /// <summary>
+  /// A MySQL Server instance that can be reached through a <see cref="MySqlWorkbenchConnection"/>.
+  /// </summary>
+  [Serializable]
+  public class MySQLInstance
+  {
+    /// <summary>
+    /// Default monitoring interval in seconds for a MySQL instance, set to 10 minutes.
+    /// </summary>
+    public const int DEFAULT_MONITORING_INTERVAL = 600;
+
+    #region Fields
+
+    /// <summary>
+    /// The Id of the <see cref="MySqlWorkbenchConnection"/> connection.
+    /// </summary>
+    private string _connectionId;
+
+    #endregion Fields
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MySQLInstance"/> class.
+    /// </summary>
+    public MySQLInstance()
+    {
+      _connectionId = string.Empty;
+      MonitorAndNotifyStatus = true;
+      MonitoringInterval = DEFAULT_MONITORING_INTERVAL;
+      UpdateTrayIconOnStatusChange = true;
+      WorkbenchConnection = null;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MySQLInstance"/> class.
+    /// </summary>
+    /// <param name="workbenchConnection">A <see cref="MySqlWorkbenchConnection"/> object.</param>
+    public MySQLInstance(MySqlWorkbenchConnection workbenchConnection)
+      : this()
+    {
+      WorkbenchConnection = workbenchConnection;
+      ConnectionId = workbenchConnection != null ? workbenchConnection.Id : string.Empty;
+    }
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the Id of the <see cref="MySqlWorkbenchConnection"/> connection.
+    /// </summary>
+    [XmlAttribute(AttributeName = "ConnectionId")]
+    public string ConnectionId
+    {
+      get
+      {
+        return _connectionId;
+      }
+
+      set
+      {
+        _connectionId = value;
+        if (!string.IsNullOrEmpty(_connectionId))
+        {
+          WorkbenchConnection = MySqlWorkbench.Connections.First(conn => conn.Id == _connectionId);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether this instance is being monitored and status changes notified to users.
+    /// </summary>
+    [XmlAttribute(AttributeName = "MonitorAndNotifyStatus")]
+    public bool MonitorAndNotifyStatus { get; set; }
+
+    /// <summary>
+    /// Gets or sets the monitoring interval in seconds for this MySQL instance.
+    /// </summary>
+    [XmlAttribute(AttributeName = "MonitoringInterval")]
+    public int MonitoringInterval { get; set; }
+
+    /// <summary>
+    ///Gets the name of the Workbench connection used to connect to the MySQL Server instance.
+    /// </summary>
+    [XmlIgnore]
+    public string Name
+    {
+      get
+      {
+        return WorkbenchConnection != null ? WorkbenchConnection.Name : string.Empty;
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether status changes of this instance trigger an update of the tray icon.
+    /// </summary>
+    [XmlAttribute(AttributeName = "UpdateTrayIconOnStatusChange")]
+    public bool UpdateTrayIconOnStatusChange { get; set; }
+
+    /// <summary>
+    /// Gets a name composed of the Workbench connection name plus additional information of the connection host.
+    /// </summary>
+    [XmlIgnore]
+    public string VerboseName
+    {
+      get
+      {
+        if (WorkbenchConnection == null)
+        {
+          return string.Empty;
+        }
+
+        string hostDescription = WorkbenchConnection.DriverType == MySqlWorkbenchConnectionType.NamedPipes ? MySqlWorkbenchConnection.DEFAULT_HOSTNAME : string.Format("{0}:{1}", WorkbenchConnection.Host, WorkbenchConnection.Port);
+        return string.Format("{0} ({1})", WorkbenchConnection.Name, hostDescription);
+      }
+    }
+
+    /// <summary>
+    /// Gets a <see cref="MySqlWorkbenchConnection"/> object with the connection properties for this instance.
+    /// </summary>
+    [XmlIgnore]
+    public MySqlWorkbenchConnection WorkbenchConnection { get; private set; }
+
+    #endregion Properties
+  }
+}
---
+++ Source/MySQLInstancesList.cs
@@ -0,0 +1,256 @@
+﻿//
+// Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; version 2 of the
+// License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301  USA
+//
+
+namespace MySql.Notifier
+{
+  using System.Collections;
+  using System.Collections.Generic;
+  using MySql.Notifier.Properties;
+
+  /// <summary>
+  /// List of <see cref="MySQLInstance"/> objects used to monitor MySQL Server instances.
+  /// </summary>
+  public class MySQLInstancesList : IList<MySQLInstance>
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MySQLInstancesList"/> class.
+    /// </summary>
+    public MySQLInstancesList()
+    {
+      if (InstancesList == null)
+      {
+        InstancesList = new List<MySQLInstance>();
+        return;
+      }
+
+      //// TODO: Loop through instances and add them to Notifier's context menu and monitor.
+      foreach (var instance in InstancesList)
+      {
+      }
+    }
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets a list of <see cref="MySQLInstance"/> objects representing instances being monitored.
+    /// </summary>
+    private List<MySQLInstance> InstancesList
+    {
+      get
+      {
+        return Settings.Default.MySQLInstancesList;
+      }
+
+      set
+      {
+        Settings.Default.MySQLInstancesList = value;
+      }
+    }
+
+    #endregion Properties
+
+    #region IList implementation
+
+    /// <summary>
+    /// Gets the number of elements actually contained in the list.
+    /// </summary>
+    public int Count
+    {
+      get
+      {
+        return InstancesList.Count;
+      }
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the collection is read-only
+    /// </summary>
+    public bool IsReadOnly
+    {
+      get
+      {
+        return false;
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets the element at the specified index.
+    /// </summary>
+    /// <param name="index">The zero-based index of the element to get or set.</param>
+    /// <returns>A <see cref="MySQLInstance"/> object at the given index position.</returns>
+    public MySQLInstance this[int index]
+    {
+      get
+      {
+        return InstancesList[index];
+      }
+
+      set
+      {
+        InstancesList[index] = value;
+      }
+    }
+
+    /// <summary>
+    /// Adds a <see cref="MySQLInstance"/> object to the end of the list.
+    /// </summary>
+    /// <param name="item">A <see cref="MySQLInstance"/> object to add.</param>
+    public void Add(MySQLInstance item)
+    {
+      InstancesList.Add(item);
+
+      //// TODO: Monitor the connection.
+
+      //// TODO: Update the Notifier UI.
+
+      Settings.Default.Save();
+    }
+
+    /// <summary>
+    /// Removes all elements from the list.
+    /// </summary>
+    public void Clear()
+    {
+      InstancesList.Clear();
+    }
+
+    /// <summary>
+    /// Determines whether an element is in the list.
+    /// </summary>
+    /// <param name="item">A <see cref="MySQLInstance"/> object.</param>
+    /// <returns>true if item is found in the list; otherwise, false.</returns>
+    public bool Contains(MySQLInstance item)
+    {
+      return InstancesList.Contains(item);
+    }
+
+    /// <summary>
+    /// Copies the entire list to a compatible one-dimensional array, starting at the specified index of the target array.
+    /// </summary>
+    /// <param name="array">The one-dimensional <see cref="Array"/> that is the destination of the elements copied from list. The <see cref="Array"/> must have zero-based indexing.</param>
+    /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
+    public void CopyTo(MySQLInstance[] array, int arrayIndex)
+    {
+      InstancesList.CopyTo(array, arrayIndex);
+    }
+
+    /// <summary>
+    /// Returns an enumerator that iterates through the list.
+    /// </summary>
+    /// <returns>An <see cref="Enumerator"/> for the list.</returns>
+    public IEnumerator<MySQLInstance> GetEnumerator()
+    {
+      return InstancesList.GetEnumerator();
+    }
+
+    /// <summary>
+    /// Returns an enumerator that iterates through a collection.
+    /// </summary>
+    /// <returns>An <see cref="IEnumerator"/> object that can be used to iterate through the collection.</returns>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return InstancesList.GetEnumerator();
+    }
+
+    /// <summary>
+    /// Determines the index of a specific item in the list.
+    /// </summary>
+    /// <param name="item">The <see cref="MySQLInstance"/> object to locate in the list.</param>
+    /// <returns>The index of item if found in the list; otherwise, –1.</returns>
+    public int IndexOf(MySQLInstance item)
+    {
+      return InstancesList.IndexOf(item);
+    }
+
+    /// <summary>
+    /// Inserts an item to the list at the specified index.
+    /// </summary>
+    /// <param name="index">The zero-based index at which <seealso cref="item"/> should be inserted.</param>
+    /// <param name="item">The <see cref="MySQLInstance"/> object to insert to the list.</param>
+    public void Insert(int index, MySQLInstance item)
+    {
+      InstancesList.Insert(index, item);
+    }
+
+    /// <summary>
+    /// Removes the first occurrence of a specific <see cref="MySQLInstance"/> object from the list.
+    /// </summary>
+    /// <param name="item">The <see cref="MySQLInstance"/> object to remove from the list.</param>
+    /// <returns>true if item is successfully removed; otherwise, false. This method also returns false if item was not found in the list.</returns>
+    public bool Remove(MySQLInstance item)
+    {
+      int index = InstancesList.IndexOf(item);
+      bool success = index >= 0;
+      if (success)
+      {
+        try
+        {
+          RemoveAt(index);
+        }
+        catch
+        {
+          success = false;
+        }
+      }
+
+      return success;
+    }
+
+    /// <summary>
+    /// Removes the first occurrence of a specific <see cref="MySQLInstance"/> object with the given id from the list.
+    /// </summary>
+    /// <param name="connectionId">Id if the connection linked to the instance to remove.</param>
+    /// <returns>true if item is successfully removed; otherwise, false. This method also returns false if item was not found in the list.</returns>
+    public bool Remove(string connectionId)
+    {
+      int index = InstancesList.FindIndex(ins => ins.ConnectionId == connectionId);
+      bool success = index >= 0;
+      if (success)
+      {
+        try
+        {
+          RemoveAt(index);
+        }
+        catch
+        {
+          success = false;
+        }
+      }
+
+      return success;
+    }
+
+    /// <summary>
+    /// Removes the element at the specified index of the list.
+    /// </summary>
+    /// <param name="index">The zero-based index of the element to remove.</param>
+    public void RemoveAt(int index)
+    {
+      InstancesList.RemoveAt(index);
+
+      //// TODO: Stop monitoring the connection.
+
+      //// TODO: Update the Notifier UI.
+
+      Settings.Default.Save();
+    }
+
+    #endregion IList implementation
+  }
+}
